2e540b91518c9af4482d0ec97a550757ed9ea5f5
kate-zahorui <kate.zahorui@gmail.com>
2022-10-13 14:56:45 +0300

    Adds saving contacts to MongoDB

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,83 +1,29 @@
 const express = require("express");
-const Joi = require("joi");
-const contacts = require("../../models/contacts");
-const { RequestError } = require("../../helpers");
+const { validateBody } = require("../../middlewares");
+const { schemas } = require("../../models/contact");
+const { ctrlWrapper } = require("../../helpers");
+const ctrl = require("../../controllers/contacts");
 
 const router = express.Router();
 
-const addSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
-});
+router.get("/", ctrlWrapper(ctrl.getAll));
 
-router.get("/", async (req, res, next) => {
-  try {
-    const allContacts = await contacts.listContacts();
-    res.json(allContacts);
-  } catch (error) {
-    next(error);
-  }
-});
+router.get("/:id", ctrlWrapper(ctrl.getById));
 
-router.get("/:id", async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const contact = await contacts.getContactById(id);
-    if (!contact) {
-      throw RequestError(404, "Not found");
-    }
-    res.json(contact);
-  } catch (error) {
-    next(error);
-  }
-});
+router.post("/", validateBody(schemas.addSchema), ctrlWrapper(ctrl.add));
 
-router.post("/", async (req, res, next) => {
-  try {
-    const { error } = addSchema.validate(req.body);
-    if (error) {
-      throw RequestError(400, error.message);
-    }
+router.delete("/:id", ctrlWrapper(ctrl.removeById));
 
-    const newContact = await contacts.addContact(req.body);
-    res.status(201).json(newContact);
-  } catch (error) {
-    next(error);
-  }
-});
+router.put(
+  "/:id",
+  validateBody(schemas.addSchema),
+  ctrlWrapper(ctrl.updateById)
+);
 
-router.delete("/:id", async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const removedContact = await contacts.removeContact(id);
-    if (!removedContact) {
-      throw RequestError(404, "Not found");
-    }
-
-    res.json({ message: "contact deleted" });
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.put("/:id", async (req, res, next) => {
-  try {
-    const { error } = addSchema.validate(req.body);
-    if (error) {
-      throw RequestError(400, error.message);
-    }
-    const { id } = req.params;
-
-    const newContact = await contacts.updateContact(id, req.body);
-    if (!newContact) {
-      throw RequestError(404, "Not found");
-    }
-
-    res.json(newContact);
-  } catch (error) {
-    next(error);
-  }
-});
+router.patch(
+  "/:id/favorite",
+  validateBody(schemas.updateFavoriteSchema),
+  ctrlWrapper(ctrl.updateStatusContact)
+);
 
 module.exports = router;
